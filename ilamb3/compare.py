5a6eeff506374db8cb9f67f710bffbf6a5ec3677
Nathan Collier <nathaniel.collier@gmail.com>
2023-07-06 08:35:44 -0400

    rework of some functions in compare

--- ilamb3/compare.py
+++ ilamb3/compare.py
@@ -1,6 +1,6 @@
 """Functions for preparing datasets for comparison."""
 import datetime
-import warnings
+from typing import Union
 
 import numpy as np
 import xarray as xr
@@ -9,31 +9,64 @@ from ilamb3 import dataset as dset
 
 
 def nest_spatial_grids(*args):
-    """returns the arguments interpolated to a nested grid"""
+    """Return the arguments interpolated to a nested grid.
+
+    In order to avoid loss of information to interpolation, when comparing two or more
+    datasets, ILAMB uses nearest neighbor interpolation on a grid composed of the union
+    of the lat/lon breaks of all sources. See this
+    [figure](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2018MS001354#jame20779-fig-0002)
+    and surrounding discussion for more information.
+
+    Parameters
+    ----------
+    *args
+        Any number of `xr.Datasets` to be included in creating a nested grid.
+
+    Returns
+    -------
+    *args
+        The input *args interpolated to the nested grid.
+
+    """
+
+    def _return_breaks(ds: xr.Dataset, dim_name: str):
+        dim = ds[dim_name]
+        if "bounds" in dim.attrs and dim.attrs["bounds"] in ds:
+            dim = ds[dim.attrs["bounds"]]
+        else:
+            dim = ds.cf.add_bounds(dim_name)[f"{dim_name}_bounds"]
+        return dim.to_numpy().flatten()
+
+    # find the union of all the breaks, and then the centroids of this irregular grid
     lat = np.empty(0)
     lon = np.empty(0)
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore")
-        for arg in args:
-            lat = np.union1d(lat, arg[dset.get_latitude_name(arg)])
-            lon = np.union1d(lon, arg[dset.get_longitude_name(arg)])
-        out = [
-            arg.interp(lat=lat, lon=lon, method="nearest").pint.quantify(arg.pint.units)
-            for arg in args
-        ]
+    for arg in args:
+        lat = np.union1d(lat, _return_breaks(arg, dset.get_dim_name(arg, "lat")))
+        lon = np.union1d(lon, _return_breaks(arg, dset.get_dim_name(arg, "lon")))
+    lat = 0.5 * (lat[:-1] + lat[1:])
+    lon = 0.5 * (lon[:-1] + lon[1:])
+    out = []
+    for arg in args:
+        iarg = {dset.get_dim_name(arg, "lat"): lat, dset.get_dim_name(arg, "lon"): lon}
+        # the interp function will strip the units off the args, so we do some pint
+        # gymnastics to avoid warnings
+        iarg = arg.pint.dequantify().interp(iarg, method="nearest").pint.quantify()
+        # if 'bounds' existed, they will now be interpolated and incorrect
+        for dim in ["lat", "lon"]:
+            dim_name = dset.get_dim_name(iarg, dim)
+            dim = iarg[dim_name]
+            if "bounds" in dim.attrs and dim.attrs["bounds"] in iarg:
+                iarg = iarg.drop(dim.attrs["bounds"])
+        out.append(iarg)
     return out
 
 
 def is_spatially_aligned(dsa: xr.Dataset, dsb: xr.Dataset) -> bool:
-    """Are the lats and lons of dsa and dsb aligned?"""
-    alat_name = dset.get_latitude_name(dsa)
-    blat_name = dset.get_latitude_name(dsb)
-    alon_name = dset.get_longitude_name(dsa)
-    blon_name = dset.get_longitude_name(dsb)
-    if alat_name is None or blat_name is None:
-        return False
-    if alon_name is None or blon_name is None:
-        return False
+    """Are the lats and lons of dsa and dsb close to each other?"""
+    alat_name = dset.get_dim_name(dsa, "lat")
+    blat_name = dset.get_dim_name(dsb, "lat")
+    alon_name = dset.get_dim_name(dsa, "lon")
+    blon_name = dset.get_dim_name(dsb, "lon")
     if dsa[alat_name].size != dsb[blat_name].size:
         return False
     if dsa[alon_name].size != dsb[blon_name].size:
@@ -46,10 +79,39 @@ def is_spatially_aligned(dsa: xr.Dataset, dsb: xr.Dataset) -> bool:
 
 
 def pick_grid_aligned(
-    ref0: xr.Dataset, com0: xr.Dataset, ref: xr.Dataset = None, com: xr.Dataset = None
+    ref0: xr.Dataset,
+    com0: xr.Dataset,
+    ref: Union[xr.Dataset, None] = None,
+    com: Union[xr.Dataset, None] = None,
 ) -> tuple[xr.Dataset, xr.Dataset]:
-    """Pick variables for ref and com such that they are grid aligned without
-    recomputing if not needed."""
+    """Return a reference and comparison dataset that is spatially grid-aligned.
+
+    This routine implements some logic to minimize the computational burden of
+    interpolating when applying possibly many analysis routines to a given reference and
+    comparison.
+
+    - If the original versions are on the same grid, then return them.
+    - If interpolated versions are passed in, then check they are in fact the same and
+      return.
+    - Otherwise compute a nesting of the original versions and return.
+
+    Parameters
+    ----------
+    ref0
+        The original reference dataset
+    com0
+        The original comparison dataset
+    ref
+        The optional interpolated reference
+    com
+        The optional interpolated comparison
+
+    Returns
+    -------
+    ref, com
+        A reference and comparison obtained with minimal additional computational burden
+
+    """
     if is_spatially_aligned(ref0, com0):
         return ref0, com0
     if (ref is not None) and (com is not None):
@@ -65,7 +127,6 @@ def trim_time(dsa: xr.Dataset, dsb: xr.Dataset) -> tuple[xr.Dataset, xr.Dataset]
         return dsa, dsb
     if "time" not in dsb.dims:
         return dsa, dsb
-    # pylint: disable=no-member
     at0, atf = dset.time_extent(dsa)
     bt0, btf = dset.time_extent(dsb)
     tol = datetime.timedelta(days=1)  # add some padding
