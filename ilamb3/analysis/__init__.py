730e1081a55c6632e2f0ed3bf2bcf867f35647e8
Nathan Collier <nathaniel.collier@gmail.com>
2024-05-30 11:05:45 -0400

    added the nbp analysis

--- ilamb3/analysis/__init__.py
+++ ilamb3/analysis/__init__.py
@@ -1,6 +1,7 @@
 """Modular ILAMB methodology functions."""
 
 from ilamb3.analysis.bias import bias_analysis
+from ilamb3.analysis.nbp import nbp_analysis
 from ilamb3.analysis.relationship import relationship_analysis
 
-__all__ = ["bias_analysis", "relationship_analysis"]
+__all__ = ["bias_analysis", "relationship_analysis", "nbp_analysis"]
