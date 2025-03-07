68f4572ab651b04090ccc3555a85247b0f144d89
Nathan Collier <nathaniel.collier@gmail.com>
2025-03-07 08:31:27 -0500

    expand to an ocean comparison with 2d grids and depth

--- ilamb3/tests/test_run.py
+++ ilamb3/tests/test_run.py
@@ -12,53 +12,73 @@ import ilamb3.run as run
 
 
 def generate_test_dset(
-    seed: int = 1, nyear=None, nlat=None, nlon=None, name="tas", unit="degC"
+    name: str,
+    unit: str,
+    seed: int = 1,
+    nyear: int | None = None,
+    nlat: int | None = None,
+    nlon: int | None = None,
+    ndepth: int | None = None,
+    latlon2d: bool = False,
 ):
     rs = np.random.RandomState(seed)
-    coords = []
-    dims = []
-    if nyear is not None:
-        time = [
+    coords = {}
+    if nyear:
+        coords["time"] = [
             cf.DatetimeNoLeap(2000 + int(m / 12), (m % 12) + 1, 15)
             for m in range(nyear * 12)
         ]
-        coords.append(time)
-        dims.append("time")
-    if nlat is not None:
-        lat = np.linspace(-90, 90, nlat + 1)
-        lat = 0.5 * (lat[1:] + lat[:-1])
-        coords.append(lat)
-        dims.append("lat")
-    if nlon is not None:
-        lon = np.linspace(0, 360, nlon + 1)
-        lon = 0.5 * (lon[1:] + lon[:-1])
-        coords.append(lon)
-        dims.append("lon")
+    if ndepth:
+        coords["depth"] = 0.5 * (
+            np.linspace(0, 100, ndepth + 1)[1:] + np.linspace(0, 100, ndepth + 1)[:-1]
+        )
+    if nlat:
+        coords["lat"] = 0.5 * (
+            np.linspace(-90, 90, nlat + 1)[1:] + np.linspace(-90, 90, nlat + 1)[:-1]
+        )
+    if nlon:
+        coords["lon"] = 0.5 * (
+            np.linspace(0, 360, nlon + 1)[1:] + np.linspace(0, 360, nlon + 1)[:-1]
+        )
+    if latlon2d:
+        coords["j"] = np.asarray(range(nlat))
+        coords["i"] = np.asarray(range(nlon))
+        dims = [c for c in coords if c not in ["lat", "lon"]]
+        coords["lat"], coords["lon"] = np.meshgrid(coords["lat"], coords["lon"])
+        coords["lat"] = xr.DataArray(coords["lat"].T, dims=["j", "i"])
+        coords["lon"] = xr.DataArray(coords["lon"].T, dims=["j", "i"])
+    else:
+        dims = [c for c in coords]
     ds = xr.Dataset(
         data_vars={
             name: xr.DataArray(
-                rs.rand(*[len(c) for c in coords]) * 20,
+                rs.rand(*[len(coords[d]) for d in dims]) * 20,
                 coords=coords,
                 dims=dims,
+                name=name,
+                attrs={"units": unit},
             ),
         }
     )
-    ds[name].attrs["units"] = unit
     return ds
 
 
 @pytest.mark.parametrize(
-    "reference_key,score",
+    "reference_key,registry_name,score",
     [
-        ("test/Site/tas.nc", 0.1310922449190615),
-        ("test/Grid/gpp.nc", 0.0127712918632477),
+        ("test/Site/tas.nc", "ilamb", 0.1310922449190615),
+        ("test/Grid/gpp.nc", "ilamb", 0.0127712918632477),
+        ("test/thetao.nc", "iomb", 0.0622860516334433),
     ],
 )
-def test_run(reference_key: str, score: float):
-    UNITS = {"tas": "degC", "gpp": "g m-2 d-1"}
+def test_run(reference_key: str, registry_name: str, score: float):
+    UNITS = {"tas": "degC", "gpp": "g m-2 d-1", "thetao": "degC"}
     # setup test reference data
     variable_id = reference_key.split("/")[-1].replace(".nc", "")
-    registry = ilamb3.ilamb_catalog()
+    if registry_name == "ilamb":
+        registry = ilamb3.ilamb_catalog()
+    elif registry_name == "iomb":
+        registry = ilamb3.iomb_catalog()
     registry.fetch(reference_key)
     df_registry = run.registry_to_dataframe(registry)
     # setup temp dir and clean
@@ -68,7 +88,14 @@ def test_run(reference_key: str, score: float):
         fname.unlink()
     # setup some test comparison data
     com = generate_test_dset(
-        seed=1, nyear=10, nlat=18, nlon=36, name=variable_id, unit=UNITS[variable_id]
+        variable_id,
+        UNITS[variable_id],
+        seed=1,
+        nyear=10,
+        nlat=18,
+        nlon=36,
+        ndepth=2 if registry_name == "iomb" else None,
+        latlon2d=(registry_name == "iomb"),
     )
     com.to_netcdf(output_path / "tmp.nc")
     df_com = pd.DataFrame(
@@ -85,13 +112,10 @@ def test_run(reference_key: str, score: float):
     )
     # run the analysis
     ilamb3.conf.set(prefer_regional_quantiles=False, use_uncertainty=False)
-    run.run_simple(
-        df_registry,
-        reference_key,
-        df_com,
-        output_path,
-        sources={variable_id: reference_key},
-    )
+    setup = {"sources": {variable_id: reference_key}}
+    if registry_name == "iomb":
+        setup["depth"] = 10.0
+    run.run_simple(df_registry, reference_key, df_com, output_path, **setup)
 
     # are the scores correct?
     df = pd.read_csv(output_path / "Junk-r1i1p1f1-gn.csv")
