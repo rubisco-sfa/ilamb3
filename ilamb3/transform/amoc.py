f3e53662909b57b260fff2ddef6b2efe3b16d3bf
Nathan Collier <nathaniel.collier@gmail.com>
2025-06-06 11:25:07 -0400

    initial transition of transforms to ABCs

--- ilamb3/transform/amoc.py
+++ ilamb3/transform/amoc.py
@@ -1,22 +1,27 @@
 import xarray as xr
 
 import ilamb3.dataset as dset
+from ilamb3.transform import ILAMBTransform
 
 
-def msftmz_to_rapid(ds: xr.Dataset) -> xr.Dataset:
-    if "msftmz" not in ds:
+class msftmz_to_rapid(ILAMBTransform):
+    def required_variables(self) -> list[str]:
+        return ["msftmz"]
+
+    def __call__(self, ds: xr.Dataset) -> xr.Dataset:
+        if "amoc" in ds:
+            return ds
+        if "msftmz" not in ds:
+            return ds
+        lat_name = dset.get_dim_name(ds, "lat")
+        depth_name = dset.get_dim_name(ds, "depth")
+        ds["amoc"] = dset.convert(
+            ds["msftmz"]
+            .isel(basin=0)
+            .sel({lat_name: 26.5}, method="nearest")
+            .max(depth_name),
+            "Sv",
+            "msftmz",
+        )
+        ds = ds.drop_vars("msftmz")
         return ds
-    if "amoc" in ds:
-        return ds
-    lat_name = dset.get_dim_name(ds, "lat")
-    depth_name = dset.get_dim_name(ds, "depth")
-    ds["amoc"] = dset.convert(
-        ds["msftmz"]
-        .isel(basin=0)
-        .sel({lat_name: 26.5}, method="nearest")
-        .max(depth_name),
-        "Sv",
-        "msftmz",
-    )
-    ds = ds.drop_vars("msftmz")
-    return ds
--- ilamb3/transform/ohc.py
+++ ilamb3/transform/ohc.py
@@ -3,34 +3,38 @@ import xarray as xr
 
 import ilamb3
 import ilamb3.dataset as dset
+from ilamb3.transform.base import ILAMBTransform
 
 
-def ocean_heat_content(ds: xr.Dataset, reference_year: int | None = None) -> xr.Dataset:
-    if "ohc" in ds:
+class ocean_heat_content(ILAMBTransform):
+    def required_variables(self) -> list[str]:
+        return ["thetao", "volcello"]
+
+    def __call__(self, ds: xr.Dataset, reference_year: int | None = None) -> xr.Dataset:
+        if "ohc" in ds:
+            return ds
+        if not set(self.required_variables()).issubset(ds):
+            return ds
+        time_name = dset.get_dim_name(ds, "time")
+        if reference_year is None:
+            reference_year = int(ds[time_name].min().dt.year.values)
+        ds = ds.sel({time_name: slice(f"{reference_year}-01", None)})
+        ds = ds.pint.quantify()
+        Cp = 3991.868 * ilamb3.units.J / ilamb3.units.kg / ilamb3.units.K
+        rho = 1026.0 * ilamb3.units.kg / ilamb3.units.m**3
+        ohc = (Cp * rho * ds["thetao"] * ds["volcello"]).sum(dim=ds["volcello"].dims)
+        ohc = ohc - ohc.isel({time_name: 0})
+        ohc = ohc.pint.to("ZJ")
+        ohc.load()
+        ohc.name = "ohc"
+        ohc.attrs = {
+            "standard_name": "Ocean Heat Content",
+            "comment": f"Relative to {reference_year}",
+        }
+        ds = ds.drop_vars(["thetao", "volcello"], errors="ignore")
+        ds["ohc"] = ohc
+        ds = ds.pint.dequantify()
+        ds = ds.groupby(f"{time_name}.year").mean()
+        ds = ds.rename(year="time")
+        ds["time"] = [cf.DatetimeNoLeap(y, 6, 1) for y in ds["time"]]
         return ds
-    required_variables = ["thetao", "volcello"]
-    if not set(required_variables).issubset(ds):
-        return ds
-    time_name = dset.get_dim_name(ds, "time")
-    if reference_year is None:
-        reference_year = int(ds[time_name].min().dt.year.values)
-    ds = ds.sel({time_name: slice(f"{reference_year}-01", None)})
-    ds = ds.pint.quantify()
-    Cp = 3991.868 * ilamb3.units.J / ilamb3.units.kg / ilamb3.units.K
-    rho = 1026.0 * ilamb3.units.kg / ilamb3.units.m**3
-    ohc = (Cp * rho * ds["thetao"] * ds["volcello"]).sum(dim=ds["volcello"].dims)
-    ohc = ohc - ohc.isel({time_name: 0})
-    ohc = ohc.pint.to("ZJ")
-    ohc.load()
-    ohc.name = "ohc"
-    ohc.attrs = {
-        "standard_name": "Ocean Heat Content",
-        "comment": f"Relative to {reference_year}",
-    }
-    ds = ds.drop_vars(["thetao", "volcello"], errors="ignore")
-    ds["ohc"] = ohc
-    ds = ds.pint.dequantify()
-    ds = ds.groupby(f"{time_name}.year").mean()
-    ds = ds.rename(year="time")
-    ds["time"] = [cf.DatetimeNoLeap(y, 6, 1) for y in ds["time"]]
-    return ds
