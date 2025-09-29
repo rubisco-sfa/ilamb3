f88ba82fcd5d0d2328e65e168d15d926d2f06ddd
Nathan Collier <nathaniel.collier@gmail.com>
2025-09-29 10:04:17 -0400

    add an expression transform

--- ilamb3/transform/__init__.py
+++ ilamb3/transform/__init__.py
@@ -1,4 +1,5 @@
 from ilamb3.transform.amoc import msftmz_to_rapid
+from ilamb3.transform.expression import expression
 from ilamb3.transform.gradient import depth_gradient
 from ilamb3.transform.ohc import ocean_heat_content
 from ilamb3.transform.runoff_sensitivity import runoff_sensitivity
@@ -7,13 +8,14 @@ from ilamb3.transform.soilmoisture import soil_moisture_to_vol_fraction
 from ilamb3.transform.stratification_index import stratification_index
 
 ALL_TRANSFORMS = {
-    "soil_moisture_to_vol_fraction": soil_moisture_to_vol_fraction,
+    "depth_gradient": depth_gradient,
+    "expression": expression,
     "msftmz_to_rapid": msftmz_to_rapid,
-    "stratification_index": stratification_index,
     "ocean_heat_content": ocean_heat_content,
+    "runoff_sensitivity": runoff_sensitivity,
     "select_depth": select_depth,
     "select_time": select_time,
-    "depth_gradient": depth_gradient,
-    "runoff_sensitivity": runoff_sensitivity,
+    "soil_moisture_to_vol_fraction": soil_moisture_to_vol_fraction,
+    "stratification_index": stratification_index,
 }
 __all__ = list(ALL_TRANSFORMS.keys())
