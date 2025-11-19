d2bf92d9fc287ae738f0fb7e5c652df5c017f206
Nathan Collier <nathaniel.collier@gmail.com>
2025-11-19 13:57:43 -0500

    test updates

--- ilamb3/tests/test_nbp.py
+++ ilamb3/tests/test_nbp.py
@@ -3,7 +3,7 @@ import pytest
 import xarray as xr
 
 from ilamb3.analysis import nbp_analysis
-from ilamb3.exceptions import MissingVariable, TemporalOverlapIssue
+from ilamb3.exceptions import TemporalOverlapIssue
 from ilamb3.tests.test_compare import generate_test_dset
 
 
@@ -20,15 +20,18 @@ def test_nbp():
     )
     ref["nbp"].attrs["bounds"] = "nbp_bnds"
     com = generate_test_dset(seed=3, **grid).rename_vars({"da": "nbp"})
+    ref["nbp"].attrs["units"] = "Pg yr-1"
+    ref["nbp_bnds"].attrs["units"] = "Pg yr-1"
+    com["nbp"].attrs["units"] = "Pg yr-1"
     analysis = nbp_analysis()
     df, _, _ = analysis(ref, com)
     df = df[df["type"] == "score"]
     assert len(df) == 2
     assert np.allclose(
-        df[df["name"] == "Difference Score"].iloc[0]["value"], 2.7143968145915746e-08
+        df[df["name"] == "Difference Score"].iloc[0]["value"], 0.8815878925867227
     )
     assert np.allclose(
-        df[df["name"] == "Trajectory Score"].iloc[0]["value"], 4.481852371926836e-08
+        df[df["name"] == "Trajectory Score"].iloc[0]["value"], 1.2568309976103773e-06
     )
 
 
@@ -52,22 +55,3 @@ def test_fail():
     analysis = nbp_analysis()
     with pytest.raises(TemporalOverlapIssue):
         analysis(ref, com)
-
-
-def test_names():
-    grid = dict(ntime=36)
-    ref = generate_test_dset(**grid).rename_vars({"da": "nbp"})
-    uncert = generate_test_dset(seed=2, **grid)["da"] * 1e-2
-    ref["nbp_bnds"] = xr.DataArray(
-        np.array(
-            [ref["nbp"].values - uncert.values, ref["nbp"].values + uncert.values]
-        ).T,
-        coords={"time": ref["time"]},
-        dims=["time", "nb"],
-    )
-    ref["nbp"].attrs["bounds"] = "nbp_bnds"
-    com = generate_test_dset(seed=3, **grid)
-    analysis = nbp_analysis()
-    analysis(ref, com.rename_vars({"da": "netAtmosLandCO2Flux"}))
-    with pytest.raises(MissingVariable):
-        analysis(ref, com.rename_vars({"da": "blah"}))
