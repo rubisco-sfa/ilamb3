ac58942566f634b6bdfe1e57ae64bbc3cee2992b
Nathan Collier <nathaniel.collier@gmail.com>
2025-11-19 13:28:12 -0500

    nbp analysis refresh

--- ilamb3/analysis/nbp.py
+++ ilamb3/analysis/nbp.py
@@ -14,9 +14,38 @@ import pandas as pd
 import xarray as xr
 
 import ilamb3
+import ilamb3.plot as ilplt
+from ilamb3 import compare as cmp
 from ilamb3 import dataset as dset
 from ilamb3.analysis.base import ILAMBAnalysis
-from ilamb3.exceptions import MissingVariable, TemporalOverlapIssue
+from ilamb3.exceptions import NoUncertainty, TemporalOverlapIssue
+
+
+def _convert_carbon_units(ds):
+    for var, da in ds.items():
+        if "units" not in da.attrs:
+            continue
+        if ilamb3.units(da.attrs["units"]).check("[mass] / [time]"):
+            ds = dset.convert(ds, "Pg yr-1", varname=var)
+            ds[var] = -ds[var]
+    return ds
+
+
+def _carbon_accumulation(ds):
+    for var, da in ds.items():
+        if "units" not in da.attrs:
+            continue
+        if ilamb3.units(da.attrs["units"]).check("[mass] / [time]"):
+            ds[f"a{var}"] = da.cumsum(dim="year")
+            ds[f"a{var}"].attrs["units"] = "Pg"
+            if "ancillary_variables" in da.attrs:
+                ds[f"a{var}"].attrs[
+                    "ancillary_variables"
+                ] = f"a{da.attrs['ancillary_variables']}"
+            if "bounds" in da.attrs:
+                ds[f"a{var}"].attrs["bounds"] = f"a{da.attrs['bounds']}"
+
+    return ds
 
 
 class nbp_analysis(ILAMBAnalysis):
@@ -79,83 +108,68 @@ class nbp_analysis(ILAMBAnalysis):
         xr.Dataset
             A dataset containing comparison grided information from the comparison.
         """
-        # Default year to evaluate if none given
+        # We do not want to do the normal ILAMB maximal temporal overlap. Since
+        # we are accumulating, the comparison needs to cover the reference but
+        # we will use ILAMB functions and verify after the fact.
+        t0, tf = dset.get_time_extent(ref, include_bounds=False)
         if self.evaluation_year is None:
-            self.evaluation_year = int(ref["time"][-1].dt.year)
-
-        # Check that the comparison starts in the appropriate year
-        if com["time"][0].dt.year > ref["time"][0].dt.year:
-            raise TemporalOverlapIssue()
-        tstart = min([t for t in com["time"] if t.dt.year == ref["time"][0].dt.year])
-        tend = max([t for t in com["time"] if t.dt.year == self.evaluation_year])
-        com = com.sel({"time": slice(tstart, tend)})
-
-        # Fixes to data names and checks for required variables
-        if "netAtmosLandCO2Flux" in com:
-            com = com.rename_vars(dict(netAtmosLandCO2Flux="nbp"))
-        if "nbp" not in com:
-            msg = "`nbp` or `netAtmosLandCO2Flux` needs to be in the `com` Dataset."
-            raise MissingVariable(msg)
-        for var in ref:
-            if not var.startswith("nbp"):
-                ref = ref.drop_vars(var)
-            else:
-                ref[var] = -ref[var]
-        com["nbp"] = -com["nbp"]
-
-        # Integrate globally
-        if dset.is_spatial(com):
-            com["nbp"] = dset.integrate_space(com, "nbp")
+            self.evaluation_year = int(tf.dt.year)
+        ref, com = cmp.trim_time(ref, com)
+        ref.load()
         com.load()
-
-        # Accumulate fluxes
-        def _cumsum(ds):
-            for var, da in ds.items():
-                da = da.pint.quantify()
-                if da.pint.units is None:
-                    continue
-                if not (1.0 * da.pint.units).check("[mass] / [time]"):
-                    continue
-                da = dset.accumulate_time(ds, var)
-                da = dset.convert(da, "Pg")
-                ds[var] = da
-            return ds
-
-        ref = _cumsum(ref)
-        com = _cumsum(com)
 
         # Coarsen to annual
         ref = dset.coarsen_annual(ref)
         com = dset.coarsen_annual(com)
 
+        # Trim to the evaluation year
+        ref = ref.sel(year=slice(None, self.evaluation_year))
+        com = com.sel(year=slice(None, self.evaluation_year))
+
+        # If after trimming we don't span the original time frame, we cannot do this analysis
+        if com["year"].min() != t0.dt.year:
+            raise TemporalOverlapIssue(
+                "Comparison dataset not defined at reference beginning."
+            )
+        if com["year"].max() < self.evaluation_year:
+            com_end_year = int(com["year"].max())
+            raise TemporalOverlapIssue(
+                f"Comparison dataset does not reach the evaluation year: {com_end_year=} < {self.evaluation_year=}"
+            )
+
+        # Convert flux units
+        ref = _convert_carbon_units(ref)
+        com = _convert_carbon_units(com)
+
+        # Add in accumulations
+        ref = _carbon_accumulation(ref)
+        com = _carbon_accumulation(com)
+
         # Trajectory score
-        ref = ref.pint.quantify()
-        out_units = f"{ref['nbp'].pint.units:~cf}"
-        ref = ref.pint.dequantify()
-        nbp_low = ref["nbp"]
-        nbp_high = ref["nbp"]
-        if "bounds" in ref["nbp"].attrs and ref["nbp"].attrs["bounds"] in ref:
-            nbp_low = ref[ref["nbp"].attrs["bounds"]][:, 0]
-            nbp_high = ref[ref["nbp"].attrs["bounds"]][:, 1]
-        uncert = xr.DataArray(
-            np.sqrt(
-                (ref["nbp"] - nbp_low).values ** 2, (nbp_high - ref["nbp"]).values ** 2
-            ),
-            coords={"year": ref["year"]},
-        )
-        eps = (com["nbp"] - nbp_low).clip(0) + (nbp_high - com["nbp"]).clip(0)
+        uncert = dset.get_scalar_uncertainty(ref, "nbp")
+        bounds = dset.get_interval_uncertainty(ref, "nbp")
+        bnd_dim = bounds.dims[-1]
+        # Only count errors outside of the envelope for scoring
+        eps = (com["nbp"] - bounds.min(dim=bnd_dim)).clip(0) + (
+            bounds.max(dim=bnd_dim) - com["nbp"]
+        ).clip(0)
         traj_score = np.exp(-eps / uncert)
         traj_score = float(traj_score.mean())
 
         # Difference score
         ref_val = float(ref["nbp"].sel(year=self.evaluation_year))
+        com_val = float(com["nbp"].sel(year=self.evaluation_year))
         try:
-            com_val = float(com["nbp"].sel(year=self.evaluation_year))
-        except KeyError:
-            com_val = np.nan
+            uncert = dset.get_scalar_uncertainty(ref, "anbp")
+        except NoUncertainty:
+            uncert = xr.zeros_like(ref["anbp"])
         uncert_val = float(uncert.sel(year=self.evaluation_year))
         scale = -np.log(0.5) / 1  # outside of the uncertainty window? score < 50%
-        diff_score = np.exp(-scale * np.abs(com_val - ref_val) / uncert_val)
+        diff_score = np.exp(
+            -scale
+            * np.abs(com_val - ref_val)
+            / (np.abs(ref_val) if np.abs(uncert_val) < 1e-8 else uncert_val)
+        )
 
         # Scores and scalars
         df = pd.DataFrame(
@@ -166,7 +180,7 @@ class nbp_analysis(ILAMBAnalysis):
                     "analysis": "nbp",
                     "name": f"nbp({self.evaluation_year})",
                     "type": "scalar",
-                    "units": out_units,
+                    "units": "Pg",
                     "value": ref_val,
                 },
                 {
@@ -175,7 +189,7 @@ class nbp_analysis(ILAMBAnalysis):
                     "analysis": "nbp",
                     "name": f"nbp({self.evaluation_year})",
                     "type": "scalar",
-                    "units": out_units,
+                    "units": "Pg",
                     "value": com_val,
                 },
                 {
@@ -184,7 +198,7 @@ class nbp_analysis(ILAMBAnalysis):
                     "analysis": "nbp",
                     "name": f"diff({self.evaluation_year})",
                     "type": "scalar",
-                    "units": out_units,
+                    "units": "Pg",
                     "value": com_val - ref_val,
                 },
                 {
@@ -216,26 +230,45 @@ class nbp_analysis(ILAMBAnalysis):
         com: dict[str, xr.Dataset],
     ) -> pd.DataFrame:
         # plot over the reference limits
-        vmin = ref["nbp"].min()
-        vmax = ref["nbp"].max()
-        if "bounds" in ref["nbp"].attrs and ref["nbp"].attrs["bounds"] in ref:
-            bnd_name = ref["nbp"].attrs["bounds"]
-            vmin = ref[bnd_name].min()
-            vmax = ref[bnd_name].max()
-        vmin = float(vmin)
-        vmax = float(vmax)
+        uncert = dset.get_interval_uncertainty(ref, "anbp")
         com["Reference"] = ref
-        return pd.DataFrame(
-            [
-                {
-                    "name": "accumulation",
-                    "title": "nbp_accumulation",
-                    "region": None,
-                    "source": None,
-                    "axis": plot_accumulated_nbp(com, ref, vmin=vmin, vmax=vmax),
-                }
-            ]
-        )
+        axs = [
+            {
+                "name": "accumulation",
+                "title": "nbp_accumulation",
+                "region": None,
+                "source": None,
+                "axis": plot_accumulated_nbp(
+                    com, ref, vmin=float(uncert.min()), vmax=float(uncert.max())
+                ),
+            }
+        ]
+        lim = ilplt.determine_plot_limits(com, percent_pad=0).set_index("name")
+        axs += [
+            {
+                "name": plot,
+                "title": "nbp",
+                "region": None,
+                "source": source,
+                "axis": (
+                    ilplt.plot_curve(
+                        {source: ds} | {"Reference": ref},
+                        plot,
+                        vmin=lim.loc[plot, "low"]
+                        - 0.05 * (lim.loc[plot, "high"] - lim.loc[plot, "low"]),
+                        vmax=lim.loc[plot, "high"]
+                        + 0.05 * (lim.loc[plot, "high"] - lim.loc[plot, "low"]),
+                        title=source + " - nbp fluxes",
+                    )
+                    if plot in ds
+                    else pd.NA
+                ),
+            }
+            for plot in ["nbp"]
+            for source, ds in com.items()
+            if source != "Reference"
+        ]
+        return pd.DataFrame(axs)
 
 
 def _space_labels(
@@ -268,8 +301,12 @@ def plot_accumulated_nbp(
         data_range = vmax - vmin
         fig_height = fig.get_figheight()
         pad = 0.05 * data_range
-        if "bounds" in ref["nbp"].attrs and ref["nbp"].attrs["bounds"] in ref:
-            da = ref[ref["nbp"].attrs["bounds"]]
+
+        try:
+            da = dset.get_interval_uncertainty(ref, "anbp")
+        except NoUncertainty:
+            da = None
+        if da is not None:
             ax.fill_between(
                 ref["year"],
                 da.values[:, 0],
@@ -280,7 +317,7 @@ def plot_accumulated_nbp(
             )
         y_text = _space_labels(dsd, data_range / fig_height * FONT_SIZE / 50.0)
         for key, ds in dsd.items():
-            ds["nbp"].plot(
+            ds["anbp"].plot(
                 ax=ax,
                 lw=2,
                 color=ilamb3.conf["label_colors"].get(key, "k"),
