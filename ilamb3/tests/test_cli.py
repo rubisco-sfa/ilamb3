445f25e76c8a3306d0dd01aab91a0fe4969f8471
Nathan Collier <nathaniel.collier@gmail.com>
2026-04-20 10:49:46 -0400

    add more tests

--- ilamb3/tests/test_cli.py
+++ ilamb3/tests/test_cli.py
@@ -1,6 +1,8 @@
+import os
 import tempfile
 from pathlib import Path
 
+import pandas as pd
 import yaml
 from pytest import fixture
 
@@ -43,3 +45,23 @@ def test_parse_registry_keys(ilamb_data_key):
     keys = cli.parse_registry_keys(config_file)
     assert len(keys) == 1
     assert keys[0] == ilamb_data_key
+
+
+def test_get_local_path():
+    os.environ["ILAMB_ROOT"] = tempfile.TemporaryDirectory().name
+    catalogs = [ilamb3.ilamb_catalog()]
+    local_path = cli.get_local_path("test/Site/tas.nc", catalogs)
+    assert local_path is None
+    cli.fetch_key("test/Site/tas.nc", catalogs)
+    local_path = cli.get_local_path("test/Site/tas.nc", catalogs)
+    assert local_path is not None
+    assert local_path.is_file()
+
+
+def test_form_reference_dataframe():
+    os.environ["ILAMB_ROOT"] = tempfile.TemporaryDirectory().name
+    catalogs = [ilamb3.ilamb_catalog()]
+    cli.fetch_key("test/Site/tas.nc", catalogs)
+    df = cli.form_reference_dataframe(["test/Site/tas.nc"])
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 1
