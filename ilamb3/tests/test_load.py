e37ab269783dd043eb69bb0a443e0f9b5c7e6629
Nathan Collier <nathaniel.collier@gmail.com>
2026-05-29 15:06:57 -0400

    added a test of new function

--- ilamb3/tests/test_load.py
+++ ilamb3/tests/test_load.py
@@ -1,9 +1,11 @@
+import os
 import tempfile
 from pathlib import Path
 
 import pandas as pd
 import pytest
 
+import ilamb3
 import ilamb3.load as ill
 from ilamb3.tests.test_dataset import generate_test_dset
 
@@ -25,3 +27,14 @@ def df():
 def test_add_frequency_column(df):
     df = ill.add_frequency_column(df)
     assert (df["frequency"] == "mon").all()
+
+
+def test_load_key_or_filename():
+    # setup, make sure this asset exists
+    asset = Path(ilamb3.ilamb_catalog().fetch("regions/GlobalLand.nc"))
+    ill.load_key_or_filename("regions/GlobalLand.nc")
+    ill.load_key_or_filename(str(asset.absolute()))
+    os.environ["ILAMB_ROOT"] = str(asset.parent)
+    ill.load_key_or_filename("GlobalLand.nc")
+    with pytest.raises(FileNotFoundError):
+        ill.load_key_or_filename("not_a_file.nc")
