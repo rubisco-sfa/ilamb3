6880bda7e61e69ea85ecbfb6f0ff5f653ba5467c
msteckle <morganrsteckler@gmail.com>
2026-07-14 16:12:33 -0400

    bugfix: ilamb3/compare/__init__.py __all__ list was nested and couldn't be read by api docs creator

--- ilamb3/compare/__init__.py
+++ ilamb3/compare/__init__.py
@@ -3,4 +3,4 @@ import inspect
 import ilamb3.compare.base as base
 from ilamb3.compare.base import *  # noqa
 
-__all__ = [[f[0] for f in inspect.getmembers(base) if inspect.isfunction(f[1])]]
+__all__ = [f[0] for f in inspect.getmembers(base) if inspect.isfunction(f[1])]
