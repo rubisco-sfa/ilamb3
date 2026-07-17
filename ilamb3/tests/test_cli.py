a2b90a7dc79962f5e5968356cc9c50f43de41701
Nathan Collier <nathaniel.collier@gmail.com>
2026-07-17 14:20:35 -0400

    added a couple cli tests, more will take a larger rework

--- ilamb3/tests/test_cli.py
+++ ilamb3/tests/test_cli.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import tempfile
 from pathlib import Path
 
@@ -65,3 +66,83 @@ def test_form_reference_dataframe():
     df = cli.form_reference_dataframe(["test/Site/tas.nc"])
     assert isinstance(df, pd.DataFrame)
     assert len(df) == 1
+
+
+@fixture
+def test_config() -> Path:
+    out = {
+        "Temperature": {"sources": {"tas": "test/Site/tas.nc"}, "analyses": ["bias"]}
+    }
+    config_file = Path(tempfile.gettempdir()) / "ilamb-cli-tests/test.yaml"
+    config_file.parent.mkdir(parents=True, exist_ok=True)
+    with open(str(config_file), "w") as f:
+        f.write(yaml.dump(out))
+    return config_file
+
+
+@fixture
+def modeldb() -> Path:
+    cat = ilamb3.ilamb_catalog()
+    df = pd.DataFrame(
+        [
+            {
+                "source_id": "test",
+                "member_id": "test",
+                "grid_label": "test",
+                "variable_id": "tas",
+                "table_id": "Amon",
+                "path": cat.fetch("test/Site/tas.nc"),
+            }
+        ]
+    )
+    db_file = Path(tempfile.gettempdir()) / "ilamb-cli-tests/test.csv"
+    db_file.parent.mkdir(parents=True, exist_ok=True)
+    df.to_csv(str(db_file))
+    return db_file
+
+
+def test_cli_run(test_config, modeldb):
+    subprocess.run(
+        [
+            "ilamb",
+            "run",
+            str(test_config),
+            "--model-db",
+            str(modeldb),
+            "--output-path",
+            str(test_config.parent),
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    assert "index.html" in [
+        f.name for f in (test_config.parent / "Temperature").glob("*.html")
+    ]
+
+
+def test_cli_fetch(test_config, modeldb):
+    subprocess.run(
+        [
+            "ilamb",
+            "fetch",
+            str(test_config),
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    cat = ilamb3.ilamb_catalog()
+    assert (Path(cat.abspath) / "test/Site/tas.nc").is_file()
+
+
+def test_cli_init():
+    subprocess.run(
+        [
+            "ilamb",
+            "init",
+        ],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
